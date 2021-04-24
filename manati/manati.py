d7c910616cebc3a5ce674c906d156be0bea54259
Matthias Baer <matthias.baer@mpl.mpg.de>
2021-04-24 18:32:37 +0200

    First test for creating project

--- manati/manati.py
+++ manati/manati.py
@@ -3,6 +3,12 @@ from manati.create import create_project
 from manati.apropos import help_tests, help_install
 
 
+def validate_project_name(ctx, param, value):
+    if ' ' in value:
+        raise click.BadParameter('Whitespace in project name not allowed.')
+    return value
+
+
 @click.group('manati')
 def cli(*args, **kwargs):
     pass
@@ -14,12 +20,15 @@ def create(*args, **kwargs):
 
 
 @create.command('project')
-@click.argument('project_name')
-def create_project_command(project_name):
+@click.option('-n', '--name', 'name', required=True, callback=validate_project_name, prompt='Project name')
+#@click.option('--no-git', is_flag=True, default=False)
+#@click.option('--no-docs', is_flag=True, default=False)
+def create_project_command(name):
     try:
-        create_project(project_name)
+        #click.echo(name)
+        create_project(name)
     except Exception as e:
-        print(e)
+        click.echo(e)
 
 
 @cli.group('apropos')
